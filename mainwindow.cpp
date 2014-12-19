f6d3bd64a89228ef33d5159288b37f3510f757cc
l20074765 <y20074765.163.com>
2014-12-19 18:28:29 +0800

    sencond

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QtGui>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Qsang");
 }
 
 MainWindow::~MainWindow()
