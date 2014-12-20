296312a748a01544b25bbd89c05fbf30fb60a74a
l20074765 <y20074765.163.com>
2014-12-20 14:14:16 +0800

    Signed-off-by: l20074765 <y20074765.163.com>

--- src/dialog/mainwindow.h
+++ src/dialog/mainwindow.h
@@ -3,22 +3,16 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class MainWindow;
-}
-
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+signals:
 
-
+public slots:
 };
 
 #endif // MAINWINDOW_H
